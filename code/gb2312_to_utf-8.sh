a0fa8afb312d765c4af2bc8c1290df37d50579c7
think3r <think3r@foxmail.com>
2020-09-04 14:57:48 +0800

    add utf-8 多种代码格式支持

--- code/gb2312_to_utf-8.sh
+++ code/gb2312_to_utf-8.sh
@@ -1,5 +1,5 @@
 # [将一个目录下的 GB2312 文件批量转换为 UTF-8 的方法](https://www.cnblogs.com/bamanzi/archive/2012/06/12/convert-all-gb2312-files-to-utf-8.html)
-files=`find . -name '*.txt' | xargs enca -L zh | grep GB2312 | cut -d: -f1`
+files=`find . -name '*.txt' -o -name '*.c' -o -name '*h' -o -name '*.cpp' -o -name '*.hpp' -o -name 'Makefile*'| xargs enca -L zh | grep GB2312 | cut -d: -f1`
 for f in $files; do
 	iconv -f GBK -t UTF-8 $f > $f.utf && mv -f $f.utf $f && echo "$f done"    
 done
